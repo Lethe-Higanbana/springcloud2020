06a7965ec2723d65717aaa9baf1416df087d1ed8
zengfanbin <lethe_higanbana@sina.com>
2020-04-23 23:17:07 +0800

    Hystrix压测存在的性能问题

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/summersky/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,20 @@
+package com.summersky.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @author Lenovo
+ * @Authur:zengfanbin
+ * @Date:2020-4-23
+ * @Time:22:49
+ * @Description:
+ */
+@SpringBootApplication
+@EnableFeignClients
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class,args);
+    }
+}
---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/summersky/springcloud/service/OrderHystrixService.java
@@ -0,0 +1,36 @@
+package com.summersky.springcloud.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @author Lenovo
+ * @Authur:zengfanbin
+ * @Date:2020-4-23
+ * @Time:22:52
+ * @Description:
+ * @component:（把普通pojo实例化到spring容器中，相当于配置文件中的<bean id="" class=""/>）
+ */
+@Component
+@FeignClient(value = "CLOUD-PROVIDER-HYSTRIX-PAYMENT")
+public interface OrderHystrixService {
+    /**
+     * 正常访问
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/ok/{id}")
+    String paymentInfo_OK(@PathVariable("id") Integer id);
+
+    /**
+     * 超时访问
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/payment/hystrix/timeout/{id}")
+    String paymentInfo_TimeOut(@PathVariable("id") Integer id);
+
+}
