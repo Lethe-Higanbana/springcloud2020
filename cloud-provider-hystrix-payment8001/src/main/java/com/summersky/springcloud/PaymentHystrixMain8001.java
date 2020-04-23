bb21dcddc93a81309de3a5151f70645f1eaad0ab
zengfanbin <lethe_higanbana@sina.com>
2020-04-23 21:49:00 +0800

    Hystrix服务构建

---
+++ cloud-provider-hystrix-payment8001/src/main/java/com/summersky/springcloud/PaymentHystrixMain8001.java
@@ -0,0 +1,20 @@
+package com.summersky.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Lenovo
+ * @Authur:zengfanbin
+ * @Date:2020-4-23
+ * @Time:19:43
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentHystrixMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentHystrixMain8001.class,args);
+    }
+}
