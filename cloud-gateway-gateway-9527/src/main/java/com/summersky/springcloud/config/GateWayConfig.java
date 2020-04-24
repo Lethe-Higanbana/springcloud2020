aa2252b35b9a75505e26f84396df8ff447b76e03
zengfanbin <lethe_higanbana@sina.com>
2020-04-24 21:05:14 +0800

    Gateway路由匹配的两种方式yml和编码

---
+++ cloud-gateway-gateway-9527/src/main/java/com/summersky/springcloud/config/GateWayConfig.java
@@ -0,0 +1,35 @@
+package com.summersky.springcloud.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Lenovo
+ * @Authur:zengfanbin
+ * @Date:2020-4-24
+ * @Time:20:53
+ * @Description:
+ */
+@Configuration
+public class GateWayConfig {
+    /**
+     * 配置了一个id为route-name的路由规则
+     * 当访问localhost:9527/guonei的时候，将会转发至https://news.baidu.com/guonei
+     *
+     * @param routeLocatorBuilder
+     * @return
+     */
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        return routes.route("path_route_atguigu", r -> r.path("/guonei").uri("https://news.baidu.com/guonei")).build();
+    }
+
+    @Bean
+    public RouteLocator customRouteLocator2(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+        return routes.route("path_route_atguigu2", r -> r.path("/guoji").uri("https://news.baidu.com/guoji")).build();
+    }
+}
