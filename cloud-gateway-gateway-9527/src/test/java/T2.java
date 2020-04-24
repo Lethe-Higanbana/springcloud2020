e0deef9647483d40b8e80d64be9379c1cc001d24
zengfanbin <lethe_higanbana@sina.com>
2020-04-24 22:09:11 +0800

    Gateway路由规则

---
+++ cloud-gateway-gateway-9527/src/test/java/T2.java
@@ -0,0 +1,15 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @Authur:zengfanbin
+ * @Date:2020-4-24
+ * @Time:21:48
+ * @Description:
+ */
+public class T2 {
+    public static void main(String[] args) {
+        // 默认时区
+        ZonedDateTime now = ZonedDateTime.now();
+        System.out.println(now);
+    }
+}
