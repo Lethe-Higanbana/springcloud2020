51798c242a4b4aa0b3b875b2329021c754a1a53c
zengfanbin <lethe_higanbana@sina.com>
2020-04-24 20:46:49 +0800

    Gateway网关搭建

---
+++ cloud-gateway-gateway-9527/src/main/java/com/summersky/springcloud/GateWayMain9527.java
@@ -0,0 +1,20 @@
+package com.summersky.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Lenovo
+ * @Authur:zengfanbin
+ * @Date:2020-4-24
+ * @Time:20:30
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class,args);
+    }
+}
